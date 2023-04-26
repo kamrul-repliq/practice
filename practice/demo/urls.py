ba993bec8d537144cc1becd4c8581b577313fec5
Kamrul Hasan <kamrul.h456@gmail.com>
2023-04-26 18:44:28 +0600

    List Details View Added

--- practice/demo/urls.py
+++ practice/demo/urls.py
@@ -1,3 +1,9 @@
-from django.urls import re_path
+from django.urls import path
+from demo import views
 
-urlpatterns = []
+urlpatterns = [
+    path("authors/", views.AuthorList.as_view(), name="authors"),
+    path("authors/<slug:slug>/", views.AuthorDetail.as_view(), name="author-details"),
+    path("books/", views.BookList.as_view(), name="books"),
+    path("books/<slug:slug>/", views.BookDetail.as_view(), name="book-details"),
+]
--- practice/demo/views.py
+++ practice/demo/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+"""Views for Demo App."""
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from demo.models import Author, Book
+from demo.serializers import (
+    AuthorSerializer,
+    BookSerializer,
+)
+
+
+class AuthorList(APIView):
+    """List create API view for Author."""
+
+    serializer_class = AuthorSerializer
+
+    def get(self, request, format=None):
+        authors = Author.objects.filter()
+        serializer = self.serializer_class(authors, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AuthorDetail(APIView):
+    """Author retrieve Update destroy API VIEW."""
+
+    serializer_class = AuthorSerializer
+
+    def _get_object(self, slug):
+        return get_object_or_404(Author, slug=slug)
+
+    def get(self, request, slug, format=None):
+        author = self._get_object(slug=slug)
+        serializer = self.serializer_class(author)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, slug, format=None):
+        author = self._get_object(slug=slug)
+        serializer = self.serializer_class(author, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, slug, format=None):
+        author = self._get_object(slug=slug)
+        serializer = self.serializer_class(author, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, slug, format=None):
+        author = self._get_object(slug)
+        author.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class BookList(ListCreateAPIView):
+    """Book List create api view."""
+
+    queryset = Book.objects.filter()
+    serializer_class = BookSerializer
+
+
+class BookDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.filter()
+    serializer_class = BookSerializer
+    lookup_field = "slug"
